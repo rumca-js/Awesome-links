d5f288f0d27de3be4a979999190008882b4794b6
Rumpel Stilskin <rozbujnik@gmial.com>
2026-04-19 08:55:19 +0200

    Init

---
+++ Makefile
@@ -0,0 +1,70 @@
+# Define variables
+ARCHIVE_NAME = table.db.zip
+SOURCE_FILE = table.db
+
+# Declare phony targets
+.PHONY: zip zip-only unzip clean server pack-split unpack-split example-search example-search2 merge update
+
+# Rule to create a zip archive split into 50MB parts
+zip:
+	zip -s 50m $(ARCHIVE_NAME) $(SOURCE_FILE)
+	echo "Packed $(SOURCE_FILE) into $(ARCHIVE_NAME)"
+	rm -f $(SOURCE_FILE)
+	rm tmp.db
+
+unzip:
+	[ -e $(SOURCE_FILE) ] && rm -r $(SOURCE_FILE) || true
+	7z x $(ARCHIVE_NAME)
+
+zip-only:
+	zip -s 50m $(ARCHIVE_NAME) $(SOURCE_FILE)
+	echo "Packed $(SOURCE_FILE) into $(ARCHIVE_NAME)"
+	rm -f $(SOURCE_FILE)
+
+pack-split:
+	zip $(ARCHIVE_NAME) $(SOURCE_FILE)
+	split -b 50M -d $(ARCHIVE_NAME) $(ARCHIVE_NAME)
+	echo "Packed $(SOURCE_FILE) into $(ARCHIVE_NAME) parts"
+	rm -f $(SOURCE_FILE)
+	rm -f $(ARCHIVE_NAME)
+
+unpack-split:
+	cat internet* > $(ARCHIVE_NAME)
+	7z x $(ARCHIVE_NAME)
+	rm -f $(ARCHIVE_NAME)
+
+filter:
+	poetry run python dbfeeds.py
+	rm tmp.db
+
+# Clean rule to remove the archive
+clean:
+	rm -f $(ARCHIVE_NAME)
+	echo "Removed $(ARCHIVE_NAME)"
+
+server:
+	python3 -m http.server 8000
+
+summary:
+	poetry run python dataanalyzer.py --summary --db $(SOURCE_FILE)
+
+example-search:
+	poetry run python ./dataanalyzer.py --db internet.db --search "*Warhammer*" --tags --social --title --description --status
+example-search2:
+	poetry run python ./dataanalyzer.py --db internet.db --search "*youtube.com/channel*" --title --tags --social
+
+download-data:
+	wget https://github.com/plenaryapp/awesome-rss-feeds/archive/refs/heads/master.zip
+	7z x master.zip
+	rm master.zip
+
+merge:
+	poetry run python dbfeeds.py --convert --db places.db --output-db converted.db
+	poetry run python dbfeeds.py --merge --merge-db converted.db --old-feeds-db feeds.db --output-db feeds_new.db
+	rm feeds.db
+	mv feeds_new.db feeds.db
+	rm converted.db
+	rm places.db
+
+update:
+	poetry run python dbfeeds.py --update --db feeds.db
---
+++ scripts/config.js
@@ -0,0 +1,122 @@
+let worker = null;
+let db = null;
+let object_list_data = null;
+let system_initialized = false;
+
+let view_display_type = "search-engine";
+let view_display_style = "style-light";
+let view_show_icons = true;
+let view_small_icons = true;
+let user_age = 1;
+let debug_mode = false;
+
+let entries_direct_links = true;
+let highlight_bookmarks = false;
+let perform_auto_search = true;
+let click_behavior_modal_window = true;
+let sort_function = "-page_rating_votes"; // page_rating_votes, date_published
+let default_page_size = 200;
+
+let entries_visit_alpha=1.0;
+let entries_dead_alpha=0.5;
+
+
+function getFileVersion() {
+    /* Forces refresh of the file */
+    return "83";
+}
+
+
+function getSystemVersion() {
+    return "1.3";
+}
+
+
+function getDefaultFileName() {
+    return "table.db";
+}
+
+
+function getFileList() {
+    return [];
+}
+
+
+function getDefaultFileLocation() {
+    return "/";
+}
+
+
+function getEntryAPI() {
+   return `/api/entries`;
+}
+
+
+function getEntryLocalLink(entry) {
+    return `?entry_id=${entry.id}`;
+}
+
+
+function getHomeLocation() {
+    return "/";
+}
+
+
+function getInitialSearchSuggestsions() {
+    return ["link LIKE '%youtube.com/channel%'",
+        "link LIKE '%github.com/%'",
+        "link LIKE '%reddit.com/%'",
+        "link LIKE 'https://x.com/%'",
+        "t.tag LIKE '%search engine%'",
+        "t.tag LIKE '%operating system%'",
+        "t.tag LIKE '%interesting%'",
+        "t.tag LIKE '%self host%'",
+        "t.tag LIKE '%programming language%'",
+        "t.tag LIKE '%music artist%'",
+        "t.tag LIKE '%music band%'",
+        "t.tag LIKE '%video games%'",
+        "t.tag LIKE '%video game%'",
+        "t.tag LIKE '%wtf%'",
+        "t.tag LIKE '%funny%'",
+    ];
+}
+
+
+function getViewStyles() {
+    return [
+        "standard",
+        "gallery",
+        "search-engine",
+        "content-centric",
+        "accordion",
+        "links-only",
+    ];
+}
+
+
+function getOrderPossibilities() {
+    return [
+        ['page_rating_votes', "Votes ASC"],
+        ['-page_rating_votes', "Votes DESC"],
+        ['view_count', "Views ASC"],
+        ['-view_count', "Views DESC"],
+        ['date_published', "Date published ASC"],
+        ['-date_published', "Date published DESC"],
+        ['followers_count', "Followers ASC"],
+        ['-followers_count', "Followers DESC"],
+        ['stars', "Stars ASC"],
+        ['-stars', "Stars DESC"],
+    ];
+}
+
+
+function notify(text) {
+    console.log(text);
+}
+
+
+function debug(text) {
+    if (debug_mode) {
+    console.log(text);
+    }
+}
